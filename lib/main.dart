57d848b81f957ee8535068cca7fd875cef47f6c5
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-04-10 22:19:27 +0530

    feat: video call enabling ui

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 60),
+            child: Container(
+              height: 600,
+              width: 900,
+              decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+              child: Padding(
+                  padding:
+                      const EdgeInsets.only(top: 200, left: 150, right: 150),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Enter ID',
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey)),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green))),
+                      ),
+                      SizedBox(height: 70),
+                      Container(
+                          height: 50,
+                          width: 180,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.green),
+                              onPressed: () {},
+                              child: Text(
+                                'Join Call',
+                                style: TextStyle(fontSize: 26),
+                              )))
+                    ],
+                  )),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
